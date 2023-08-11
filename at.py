909341c8856fe5e335ff04ab1a949267e1bcf638
unknown <https://github.com/kakacarlos2324/atividade03.git>
2023-08-11 19:48:51 -0300

    atividade

---
+++ at.py
@@ -0,0 +1,8 @@
+n1 = float(input("informe um numero?"))
+n2 = float(input("informe um numero?"))
+if n1 > n2:
+    print("{}, e maior numero".format(n1))
+else:
+    n1 < n2
+    print("{}, e maior numero".format(n2))
+    
